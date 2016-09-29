0ddfddd4a86f506dacd2306a5a18c6bead3096ef
Tomas Kubica <tkubica12@gmail.com>
2016-09-28 23:56:02 -0700

    Initial release

---
+++ README.md
@@ -0,0 +1,1 @@
+This is very simple web service written in Go to demonstrate cloud, load-balancing, containers. It is compiled to Linux binary package running on port 3000. Web server returns MAC address of host server. V2 is the same, but string V2 is added (this is to demonstrate A/B testing or releases).
---
+++ web_v2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+   "github.com/codegangsta/martini"
+   "net"
+)
+
+func main() {
+  m := martini.Classic()
+  interfaces, err := net.Interfaces()
+  if err != nil {
+    panic("Unable to get interfaces.")
+  }
+  mac := interfaces[1].HardwareAddr.String()
+
+  m.Get("/", func() string {
+    return "<h1>V2: My MAC address is " + mac + "</h1>"
+  })
+
+  m.Run()
+}
